38f719cd93c036866e9286a5906ececd5618689b
naseem <naseem@knaseem-mbp.attlocal.net>
2016-08-11 11:46:47 -0400

    Initial Commit - Skeleton of App and design

---
+++ Contacts.swift
@@ -0,0 +1,17 @@
+//
+//  Contacts.swift
+//  taskCoreData
+//
+//  Created by naseem on 11/08/2016.
+//  Copyright © 2016 naseem. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Contacts: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
